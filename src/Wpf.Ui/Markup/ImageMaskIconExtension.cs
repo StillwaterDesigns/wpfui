9fc1a0394c19d13713a82cc62ce01651e754e1ed
Ian Oswald <ioswald@outlook.com>
2025-06-12 09:39:08 -0500

    - Added Image mask icon extension for initializing image source from xaml.

---
+++ src/Wpf.Ui/Markup/ImageMaskIconExtension.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+using Wpf.Ui.Controls;
+
+namespace Wpf.Ui.Markup;
+
+/// <summary>
+/// Custom <see cref="MarkupExtension"/> which can provide <see cref="ImageIcon"/>.
+/// </summary>
+/// <example>
+/// <code lang="xml">
+/// &lt;ui:Button
+///     Appearance="Primary"
+///     Content="WPF UI button with font icon"
+///     Icon="{ui:ImageIcon '/my-icon.png'}" /&gt;
+/// </code>
+/// <code lang="xml">
+/// &lt;ui:Button Icon="{ui:ImageIcon 'pack://application:,,,/Assets/wpfui.png'}" /&gt;
+/// </code>
+/// <code lang="xml">
+/// &lt;ui:HyperlinkButton Icon="{ui:ImageIcon 'pack://application:,,,/Assets/wpfui.png'}" /&gt;
+/// </code>
+/// <code lang="xml">
+/// &lt;ui:TitleBar Icon="{ui:ImageIcon 'pack://application:,,,/Assets/wpfui.png'}" /&gt;
+/// </code>
+/// </example>
+[ContentProperty(nameof(Source))]
+[MarkupExtensionReturnType(typeof(ImageMaskIcon))]
+public class ImageMaskIconExtension : MarkupExtension {
+	public ImageMaskIconExtension(ImageSource? source) {
+		Source = source;
+	}
+
+	[ConstructorArgument("source")]
+	public ImageSource? Source { get; set; }
+
+	public double Width { get; set; } = 16D;
+
+	public double Height { get; set; } = 16D;
+
+	public override object ProvideValue(IServiceProvider serviceProvider) {
+		var imageIcon = new ImageMaskIcon {
+			Source = Source,
+			Width = Width,
+			Height = Height
+		};
+
+		return imageIcon;
+	}
+}
